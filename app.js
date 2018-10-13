86d04a022ce25a9558a36c778e190d2e127fe6a9
Karl Polintan <kf.polintan@gmail.com>
2018-10-13 11:20:44 -0700

    Working on random image picker

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ Each image cannot repeat after another. There will be counters for each time an 
 
 After 25 votes, the results will be displayed.
 
+STRETCH -For randomly picking a starting image:
+use document.getElementbyId('left/mid/right').setAttribute('src',srcArray[x])
+
 */
 
 // global variables
@@ -18,6 +21,11 @@ var busImageMid = document.getElementById('mid');
 var busImageRight = document.getElementById('right');
 var imageSection = document.getElementById('clickme');
 var busImageLeftArrayIndex = 0;
+var busImageMidArrayIndex = 0;
+var busImageRightArrayIndex = 0;
+var allBusImages = [];
+var clickCounter = 0;
+
 
 // Constructor for Bus Mall Images
 var BusMallImage = function(src, name){
@@ -33,22 +41,57 @@ var BusMallImage = function(src, name){
 
 BusMallImage.prototype.renderImage = function (){
   busImageLeft.src = this.src;
-}
+};
 
 // Event listeners and handlers
 var imgClickHandler = function (eventObject) {
-  do {
-    var randomNumber = Math.floor(Math.random() * allBusImages.length)
-  } while(randomNumber === busImageLeftArrayIndex);
+  if(event.target.id === 'left' || event.target.id === 'mid' || event.target.id === 'right') {
 
-  allBusImages[busImageLeftArrayIndex].likes++;
-  allBusImages[busImageLeftArrayIndex].appeared++;
+    do {
+      var randomNumberLeft = Math.floor(Math.random() * allBusImages.length);
+    } while(randomNumberLeft === busImageLeftArrayIndex);
 
-  busImageLeftArrayIndex = randomNumber;
-  eventObject.target.src = allBusImages[randomNumber].src;
+    do {
+      var randomNumberMid = Math.floor(Math.random() * allBusImages.length);
+    } while(randomNumberMid === busImageMidArrayIndex);
+
+    do {
+      var randomNumberRight = Math.floor(Math.random() * allBusImages.length);
+    } while(randomNumberRight === busImageRightArrayIndex);
+
+    allBusImages[busImageLeftArrayIndex].likes++;
+    allBusImages[busImageLeftArrayIndex].instanced++;
+
+    busImageLeftArrayIndex = randomNumberLeft;
+    busImageMidArrayIndex = randomNumberMid;
+    busImageRightArrayIndex = randomNumberRight;
+    
+    eventObject.target.src = allBusImages[randomNumberLeft].src;
+    eventObject.target.src = allBusImages[randomNumberMid].src;
+    eventObject.target.src = allBusImages[randomNumberRight].src;
+  }
 };
 
 busImageLeft.addEventListener('click', imgClickHandler);
 
 // constructed img-objects go here
-new BusMallImage();
+new BusMallImage('./img/bag.jpg', 'Artoo Luggage');
+new BusMallImage('./img/banana.jpg', 'Nana Slicer');
+new BusMallImage('./img/bathroom.jpg', 'Bathroom iPad Stand');
+new BusMallImage('./img/boots.jpg', 'Toeless Boots');
+new BusMallImage('./img/breakfast.jpg', 'Toaster Oven & Coffee Pot');
+new BusMallImage('./img/bubblegum.jpg', 'Meatball Gum');
+new BusMallImage('./img/chair.jpg', 'Artisinal Chair');
+new BusMallImage('./img/cthulhu.jpg', 'Cthulhu Action Figure');
+new BusMallImage('./img/dragon.jpg', 'Can o\' Dragon Meats');
+new BusMallImage('./img/pen.jpg', 'Pencap Cutlery');
+new BusMallImage('./img/pet-sweep.jpg', 'PetBroom');
+new BusMallImage('./img/scissors.jpg', 'Pisa Scissors');
+new BusMallImage('./img/shark.jpg', 'Shark Sleeping Bag');
+new BusMallImage('./img/sweep.png', 'Kid Sweeper');
+new BusMallImage('./img/tauntaun.jpg', 'Tauntaun Sleeping Bag');
+new BusMallImage('./img/unicorn.jpg', 'Can o\' Unicorn Meats');
+new BusMallImage('./img/usb.gif', 'USB Tentacle');
+new BusMallImage('./img/water-can.jpg', 'Self-Watering Can');
+new BusMallImage('./img/wine-glass.jpg', 'Snifter Wine Glass');
+// add laptop steering wheel and sugar-free haribo gummy bears
